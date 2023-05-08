048a6db1294105b3281040e1fbfc60fb19320243
Jurgen Kluft <jurgen@virtuosgames.com>
2023-05-08 16:39:01 +0800

    * cbase -> ccore

--- source/main/include/cecs/c_ecs.h
+++ source/main/include/cecs/c_ecs.h
@@ -1,6 +1,6 @@
 #ifndef __CECS_ECS_H__
 #define __CECS_ECS_H__
-#include "cbase/c_target.h"
+#include "ccore/c_target.h"
 #ifdef USE_PRAGMA_ONCE
 #pragma once
 #endif
--- source/test/cpp/test_ecs.cpp
+++ source/test/cpp/test_ecs.cpp
@@ -1,4 +1,4 @@
-#include "cbase/c_target.h"
+#include "ccore/c_target.h"
 #include "cbase/c_buffer.h"
 #include "cecs/c_ecs.h"
 
